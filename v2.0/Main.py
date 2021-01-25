e965c9d0d7c96a64d00d3ee58c06c90ceb549dc0
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-01-25 16:59:33 -0300

    quito algunos archivos

---
+++ v2.0/Main.py
@@ -0,0 +1,17 @@
+import json
+import requests
+ 
+# vars from token and bot URL
+# use ENV vars!
+TOKEN = "..." # change
+URL = "https://api.telegram.org/bot" + TOKEN + "/"
+
+def main():
+    # seguir...
+
+    pass
+
+
+if __name__ == "__main__":
+    main()  
+
