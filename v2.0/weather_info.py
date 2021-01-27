3a838f2acd23384dadc43943a1e1d2c473cd8214
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-01-26 21:19:57 -0300

    Modifico salida del get_weather()

--- v2.0/weather_info.py
+++ v2.0/weather_info.py
@@ -6,27 +6,34 @@ API_KEY = os.environ["PYOWN_API_KEY"]
 
 def get_weather(city):
     id = get_id_city(city)
-    resp = requests.get("http://api.openweathermap.org/data/2.5/weather?id=" + id + "&appid=" + API_KEY)
-    resp_data = json.loads(resp.text)
-    # data processing:
-    actual_temperature = kelvin_to_celcius(resp_data.get("main").get("temp"))
-    feels_like = kelvin_to_celcius(resp_data.get("main").get("feels_like"))
-    min_temp = kelvin_to_celcius(resp_data.get("main").get("temp_min"))
-    max_temp = kelvin_to_celcius(resp_data.get("main").get("temp_max"))
-    cloudiness = str(resp_data.get("clouds").get("all")) + " %"
-    weather = translate_weather_to_spanish(resp_data.get("weather")[0].get("description"))
-    text = """
-    Datos de la ciudad de {}:
+    if id == None:
+        print("No se encontró la ciudad '{}'".format(city))
+        return "No se encontró la ciudad '{}'".format(city)
+    else:
+        resp = requests.get("http://api.openweathermap.org/data/2.5/weather?id=" + id + "&appid=" + API_KEY)
+        resp_data = json.loads(resp.text)
+        # data processing:
+        actual_temperature = kelvin_to_celcius(resp_data.get("main").get("temp"))
+        feels_like = kelvin_to_celcius(resp_data.get("main").get("feels_like"))
+        min_temp = kelvin_to_celcius(resp_data.get("main").get("temp_min"))
+        max_temp = kelvin_to_celcius(resp_data.get("main").get("temp_max"))
+        cloudiness = str(resp_data.get("clouds").get("all")) + " %"
+        weather = translate_weather_to_spanish(resp_data.get("weather")[0].get("description"))
+        text = """
+        Datos de la ciudad de {}:
     Temperatura actual: {} °C
     Sensación térmica: {} °C
     Temp. máxima: {} °C
     Temp. mínima: {} °C
     Porcentaje de nubes: {}
-    Clima actual: {}"""
-    # print(text.format(city, actual_temperature, feels_like, max_temp, min_temp, cloudiness, weather))
-    return text.format(city, actual_temperature, feels_like, max_temp, min_temp, cloudiness, weather)
-    # return "functionality out of service (Coming on v2.0)"
+    Clima actual: {}
+    
+Tox."""
+        print(text.format(city, actual_temperature, feels_like, max_temp, min_temp, cloudiness, weather))
+        return text.format(city, actual_temperature, feels_like, max_temp, min_temp, cloudiness, weather)
+        # return "functionality out of service (Coming on v2.0)"
 
+    
 def read_json_city_list():
     json_city_list = open(os.environ["PP_ROUTE"] + "/BOT_Telegram_Python/v2.0/data/city_list.json").read()
     return json.loads(json_city_list)
@@ -42,6 +49,10 @@ def get_id_city(city):
         if ((elem.get("name")).lower() == city.lower()):
             return str(elem.get("id"))
 
+def binary_search_get_city(city_list_data, objective):
+    left = 0
+    right = 0
+
 def kelvin_to_celcius(kelvin):
     return round(kelvin - 273.15)
 
@@ -56,5 +67,5 @@ def translate_weather_to_spanish(original_text):
         return text
 
 if __name__ == "__main__":
-    get_weather("montevideo")
+    get_weather("Montevideo")
 
